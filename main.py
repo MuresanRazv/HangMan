46baaed50eea825b8b3bdc7def436999e59c7ad4
seNNNz0rel <67145369+seNNNz0rel@users.noreply.github.com>
2022-03-14 22:48:27 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,162 @@
+import pygame
+import random
+import math
+# setup display
+
+pygame.init()
+WIDTH, HEIGHT = 800, 500
+win = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Hangman Game!")
+
+# button variables
+RADIUS = 20
+GAP = 15
+rectx = 280
+recty = 400
+yes = [rectx + 12, recty + 5]
+no = [rectx + 178, recty + 5]
+letters = []
+startx = round((WIDTH - (RADIUS * 2 + GAP) * 13) / 2)
+starty = 400
+A = 65
+for i in range(26):
+	x = startx + GAP * 2 + ((RADIUS * 2 + GAP) * (i % 13)) 
+	y = starty + ((i // 13) * (GAP + RADIUS * 2))
+	letters.append([x, y, chr(A + i), True])
+
+# fonts
+LETTER_FONT = pygame.font.SysFont('comicsans', 40)
+WORD_FONT = pygame.font.SysFont('comicsans', 60)
+TITLE_FONT = pygame.font.SysFont('comicsans', 70)
+
+# load images
+images = []
+for i in range(7):
+	image = pygame.image.load("hangman" + str(i) + ".png")
+	images.append(image)
+
+# colors
+WHITE = (255,255,255)
+BLACK = (0, 0, 0)
+
+# setup game loop
+FPS = 60
+clock = pygame.time.Clock()
+run = True
+
+# draw buttons
+def draw():
+	win.fill(WHITE)
+	# draw title 
+	text = TITLE_FONT.render("DEVELOPER HANGMAN",1 ,BLACK)
+	win.blit(text, (WIDTH/2 - text.get_width()/2, 20))
+
+	# draw word
+	display_word = ""
+	for letter in word:
+		if letter in guessed:
+			display_word += letter + " "
+		else:
+			display_word += "_ "
+	text = WORD_FONT.render(display_word, 1, BLACK)
+	win.blit(text, (400,200))
+
+
+	for letter in letters:
+		x, y, ltr, visible = letter
+		if visible:
+			pygame.draw.circle(win, BLACK, (x, y), RADIUS, 3)
+			text = LETTER_FONT.render(ltr, 1, BLACK)
+			win.blit(text, (x - text.get_width()/2, y - text.get_height()/2))
+
+	win.blit(images[hangman_status], (150,100))
+	pygame.display.update()
+
+# draw main menu
+def draw_main():
+	win.fill(WHITE)
+	text = TITLE_FONT.render("DEVELOPER HANGMAN",1 ,BLACK)
+	win.blit(text, (WIDTH/2 - text.get_width()/2, 20))
+
+	text = WORD_FONT.render("DO YOU WANT TO PLAY HANGMAN?", 1, BLACK)
+	win.blit(text, (WIDTH / 2 - text.get_width() / 2, HEIGHT / 2))
+	
+	text = LETTER_FONT.render("YES",1 ,WHITE)
+	pygame.draw.rect(win, BLACK, ((rectx, recty), (80, 40)))
+	win.blit(text, (yes[0], yes[1]))
+	
+	text = LETTER_FONT.render("NO",1 ,WHITE)
+	pygame.draw.rect(win, BLACK, ((rectx + 160, recty), (80, 40)))
+	win.blit(text, (no[0], no[1]))
+	
+	pygame.display.update()
+
+def display_message(message):
+	win.fill(WHITE)
+	pygame.time.delay(500)
+	text = WORD_FONT.render(message, 1, BLACK)
+	win.blit(text, (WIDTH/2 - text.get_width()/2, HEIGHT/2 - text.get_height()/2))
+	pygame.display.update()
+	pygame.time.delay(3000)
+
+# game variables
+
+words = ["HELLO", "PYTHON", "PYGAME", "HAPPY"]
+word = random.choice(words)
+guessed = [] 
+hangman_status = 0	
+
+def main():
+	run = True
+
+	global hangman_status
+	
+	while run:
+		clock.tick(FPS)
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				m_x, m_y = pygame.mouse.get_pos()
+				for letter in letters:
+					x, y, ltr, visible = letter
+					if visible:
+						dis = math.sqrt((x - m_x)**2 + (y - m_y)**2)
+						if dis < RADIUS:
+							letter[3] = False
+							guessed.append(ltr)
+							if ltr not in word:
+								hangman_status += 1
+
+		draw()
+
+		won = True
+		for letter in word:
+			if letter not in guessed:
+				won = False
+				break
+
+		if won:
+			display_message("You won!")
+			break
+		
+		if hangman_status == 6:
+			display_message("You lost!")
+			break
+
+# main menu
+while run:
+	clock.tick(FPS)
+
+	for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				m_x, m_y = pygame.mouse.get_pos()
+				if m_x >= yes[0] and m_x <= yes[0] + 80 and m_y >= yes[1] and m_y <= yes[1] + 40:
+					main()
+				elif m_x >= no[0] and m_x <= no[0] + 80 and m_y >= no[1] and m_y <= no[1] + 40:
+					run = False
+	draw_main()
+pygame.quit() 
